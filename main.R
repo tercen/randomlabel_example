153129c24fd347a7aca5047d52b27273b1cee47e
fnaji <fnaji@users.noreply.github.com>
2021-10-07 11:19:28 +0200

    Update main.R
    changed the name of the returned value

--- main.R
+++ main.R
@@ -7,7 +7,7 @@ if(!ctx$op.value('seed') == "NULL") set.seed(as.integer(ctx$op.value('seed')))
 
 data.frame(
   .ci = seq(from=0, to=ctx$cschema$nRows - 1),
-  sample = runif(ctx$cschema$nRows, 0.0, 100.0)
+  random_label = runif(ctx$cschema$nRows, 0.0, 100.0)
 ) %>%
   ctx$addNamespace() %>%
   ctx$save()
